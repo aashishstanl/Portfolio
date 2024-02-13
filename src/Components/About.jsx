9b61d87c7a8a2ec3dc1510e40de1f905c3c134fe
aashishstanl <aashishstnl@gmail.com>
2024-02-14 02:22:14 +0530

    Added Experiences

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,17 +8,20 @@ function About() {
       <div className={style.About} id="about-me">
         <h2>About Me</h2>
         <p>
-          I first got into design when I decided to pursue Master's in HCI
-          degree at Georgia Institute of Technology. From then on, my passion
-          towards design had sparked greatly which allowed me to spend countless
+          I first got into development when I decided to pursue Bachleor's in
+          <strong> Computer Science Engineering</strong> from MERI College of
+          Engineering of Technology. From then on, my passion towards
+          development had sparked greatly which allowed me to spend countless
           days and night improving my skills and knowledge. In addition to my
           day-to-day work, I love writing about my personal and professional
-          experiences on Medium and regularly share my additional fun work on
-          Dribbble. Also, I've been actively engaging in various speaking
+          experiences and regularly share my additional fun work on social
+          media. Also, I've been actively engaging in various speaking
           opportunities that allowed me to share my journey throughout my
-          career. For any questions related to mentorship, portfolio feedback
-          and career advice, book time with me on Clarity. You can also reach me
-          at <a href="mailto:aashishstnl@gmail.com">aashishstnl@gmail.com</a>.
+          career. You can also reach me at{" "}
+          <a style={{ color: "skyblue" }} href="mailto:aashishstnl@gmail.com">
+            aashishstnl@gmail.com
+          </a>
+          .
         </p>
         <div className={style.profileBtn}>
           <a
