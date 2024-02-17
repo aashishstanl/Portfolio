33b7a240f5ee7fb0feb2b38e67cae53d1ad89701
aashishstanl <aashishstnl@gmail.com>
2024-02-17 13:06:23 +0530

    Added TechStack logo

--- src/Components/TechStack.jsx
+++ src/Components/TechStack.jsx
@@ -10,24 +10,40 @@ function TechStack() {
           src="https://assets.codegrip.tech/wp-content/uploads/2021/09/28165501/JavaScript_Logo2.png"
           alt="javaScript logo"
         />
-        <img src="src/Images/TechStack Logo/HTML.png" alt="HTML logo" />
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw6BaPUhJMAROwqU2rlBJwLYs2i0y6J5OSkA&usqp=CAU"
+          alt="HTML logo"
+        />
         <img src="src/Images/TechStack Logo/CSS.png" alt="CSS logo" />
         <img
           src="src/Images/TechStack Logo/BootStrap.jpeg"
           alt="Bootstrap logo"
         />
-        <img src="src/Images/TechStack Logo/React.png" alt="React logo" />
-        <img src="src/Images/TechStack Logo/Redux.png" alt="Redux logo" />
-        <img src="src/Images/TechStack Logo/Postgres.png" alt="PostgresSQL" />
         <img
-          src="src/Images/TechStack Logo/Git.png"
+          src="https://www.svgrepo.com/show/452093/redux.svg"
+          alt="React logo"
+        />
+        <img
+          src="https://www.svgrepo.com/show/452092/react.svg"
+          alt="React logo"
+        />
+
+        <img
+          src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postgresql-icon.png"
+          alt="PostgresSQL"
+        />
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz44FpQ6CBG8etCuNa5rF1tsc9k9ijmDp_tg&usqp=CAU"
+          alt="Git"
+        />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/5251/5251795.png"
           alt="version control logo"
         />
         <img
-          src="src/Images/TechStack Logo/Version control.jpeg"
-          alt="version control logo"
+          src="https://www.svgrepo.com/show/354431/tailwindcss-icon.svg"
+          alt="AWS logo"
         />
-        <img src="src/Images/TechStack Logo/AWS.png" alt="AWS logo" />
         {/* <h6>PACCAR</h6>
             <h6>SKISUI HOUSE LTD</h6>
             <h6>CARIS LIFE SCIENCE</h6>
